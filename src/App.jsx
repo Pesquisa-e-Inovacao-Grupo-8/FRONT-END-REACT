755f951a07e522f7225ce09c69f35c7c071c1ce3
Erick David da Silva <erick.silva097@sptech.school>
2026-04-25 00:11:09 -0300

    feat: adiciona widget de acessibilidade VLibras

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { useState } from "react";
 import Navbar from "./components/home/Navbar";
 import Sidebar from "./components/admin/Sidebar";
+import VLibras from "./components/utils/VLibras";
 import AgendamentosPage from "./pages/admin/Agendamentos";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,7 +10,6 @@ import Cadastro from "./pages/Cadastro";
 import Serviços from "./pages/Serviços"
 import Agendamento from "./pages/Agendamento";
 import AgendamentosUsuário from "./pages/AgendamentosUsuário";
-
 
 const LayoutNavbar = () => (
   <>
@@ -54,6 +54,7 @@ const LayoutSidebar = () => {
 export default function App() {
   return (
     <BrowserRouter>
+    <VLibras />
       <Routes>
         <Route element={<LayoutNavbar />}>
           <Route index element={<Home />} />
